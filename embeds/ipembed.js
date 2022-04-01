2098ffc2baec0c7beb5f343a307b7b10a8d159aa
cody <codygg16@gmail.com>
2022-04-01 18:27:53 +0100

    Revert!

--- embeds/ipembed.js
+++
@@ -1,8 +0,0 @@
-new Discord.MessageEmbed()
-.setColor('#008000')
-.setTitle("Pandamium Server IP's")
-.setDescription(`Release version : ${releaseVersion} \nSnapshot version : ${snapshotVersion}`)
-.addFields(
-  {name:`Release IP:`,value:`pandamium.eu`},
-  {name: `Snapshot IP:`, value: `snapshot.pandamium.eu`}
-).setTimestamp()
