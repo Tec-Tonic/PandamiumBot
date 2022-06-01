1cdf7d799178512fc0e68c74aa369ee6b7216972
cody <codygg16@gmail.com>
2022-06-01 16:10:59 +0100

    Added puntuation remover

--- commands/ipcommand.js
+++ commands/ipcommand.js
@@ -1,11 +1,19 @@
 const ipFilter = require(`../filters/ipfilter.json`);
+function removeCapPunctuation(string) {
+  return string
+    .split('')
+    .filter(function(letter) {
+      return punctuation.indexOf(letter) === -1;
+    })
+    .join('');
+}
 module.exports = {
     name: 'ip',
     execute(message,Discord,client){
       if (message.author == client.user) return;
       if (message.author.bot) return;
 
-      const filterpunctuation = message.content
+  const filterpunctuation = message.content
    let ipfoundInText = false;
    for (var i in ipFilter) {
    if (removeCapPunctuation(filterpunctuation).toLowerCase().includes(ipFilter[i])) ipfoundInText = true;
