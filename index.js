f49128cc26584715f30ccec529780b432c0f565f
cody <codygg16@gmail.com>
2022-06-01 13:56:09 +0100

    Moved index file around

--- index.js
+++ index.js
@@ -40,16 +40,7 @@ client.once('ready', () => {
       console.log(`Logged in as ${client.user.tag} on ${serverCount} servers`);
   });
 
-client.on('messageCreate', message =>{  
-  // prefix ip command
-  if(!message.content.startsWith(prefix) || message.author.bot) return;
-      const args = message.content.slice(prefix.length).split(/ +/);
-      const command = args.shift().toLowerCase();
-      if(command === 'ip'){
-        message.react('☑️')
-        client.command.get('ip').execute(message,Discord,client)
-    }
-  
+client.on('messageCreate', message =>{   
 // scam filter
   if (message.author == client.user) return;
     var content = message.content;
@@ -133,8 +124,19 @@ const { waitForDebugger } = require('inspector');
           client.command.get('ip').execute(message,Discord,client)
         return;
       }
+
+       // prefix ip command
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if(command === 'ip'){
+    message.react('☑️')
+    client.command.get('ip').execute(message,Discord,client)
+}
+
     })
 
+    
   //Announcement checks
   const wait = require('util').promisify(setTimeout);
   client.on("messageCreate", async message => {
