77184c527597659cdc80e80f30c426cfe47a600b
cody <codygg16@gmail.com>
2022-07-05 01:58:38 +0100

    fixed spelling error

--- commands/deletedmessage.js
+++ commands/deletedmessage.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message,Discord,client){
 
 const deletedMessage = message.content
-const deletedMessageAuthor = deletedMessage.author.username
+const deletedMessageAuthor = deletedMessage.author.user.username
 const deleteMsgEmbed = new Discord.MessageEmbed().setColor('#FF0000').setTitle("Message Deleted").addFields(
   {name: `Message :`, value: `${deletedMessage}`},
   {name: `Info :`, value: `${deletedMessageAuthor}`}
