84b952ba055f35cdb6c0877ef89f5f89066d7894
cody <codygg16@gmail.com>
2022-07-05 02:09:07 +0100

    fix 3

--- commands/deletedmessage.js
+++ commands/deletedmessage.js
@@ -1,13 +1,15 @@
 const personalLog = '963436191426957352'
-export const name = 'confirmDelete'
-export function execute(message, Discord, client) {
+module.exports = {
+    name: 'confirmDelete',
+    execute(message,Discord,client){
 
-    const deletedMessage = message.content
-    const deletedMessageAuthor = message.author.username
-    const deleteMsgEmbed = new Discord.MessageEmbed().setColor('#FF0000').setTitle("Message Deleted").addFields(
-        { name: `Message :`, value: `${deletedMessage}` },
-        { name: `Info :`, value: `${deletedMessageAuthor}` }
-    )
-    client.channels.cache.get(personalLog).send(({ embeds: [deleteMsgEmbed] }))
+const deletedMessage = message.content
+const deletedMessageAuthor = message.author.username
+const deleteMsgEmbed = new Discord.MessageEmbed().setColor('#FF0000').setTitle("Message Deleted").addFields(
+  {name: `Message :`, value: `${deletedMessage}`},
+  {name: `Info :`, value: `${deletedMessageAuthor}`}
+  )
+client.channels.cache.get(personalLog).send(({embeds: [deleteMsgEmbed]}))
 
+    }
 }
