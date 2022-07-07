c998b96377f5f9ceb63e31d3de5e2c90ceaf296a
cody <codygg16@gmail.com>
2022-07-07 19:03:59 +0100

    added Game version to ip command

--- commands/ipcommand.js
+++ commands/ipcommand.js
@@ -1,5 +1,8 @@
 const ipFilter = require(`../filters/ipfilter.json`);
 const fs = require('fs');
+const list = require('minecraft-server-util');
+
+//remove Punctuation
 var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 function removeCapPunctuation(string) {
   return string
@@ -9,6 +12,8 @@ function removeCapPunctuation(string) {
     })
     .join('');
 }
+
+
 module.exports = {
     name: 'ip',
     execute(message,Discord,client){
@@ -23,13 +28,19 @@ module.exports = {
    if (ipfoundInText) {
       message.react('<:resolved:942896814154199150> ')
     
-    const ip2embed = new Discord.MessageEmbed()
-      .setColor('#008000')
-      .setTitle("Pandamium Server IP's")
-      .addFields(
-        {name:`Release IP:`,value:`pandamium.eu`},
-        {name: `Snapshot IP:`, value: `snapshot.pandamium.eu`}
-        )
+
+      list.status('pandamium.eu').then((Responses) =>{
+        list.status('snapshot.pandamium.eu').then((Response) =>{
+          const snapVersionIP = Response.version.name
+          const releaseversionIP = Responses.version.name
+  
+        const ip2embed = new Discord.MessageEmbed()
+        .setColor('#008000')
+        .setTitle("Pandamium Server IP's")
+        .addFields(
+          {name:`Release IP: `,value:`pandamium.eu\n **Version:** ${releaseversionIP}\n\n`},
+          {name: `Snapshot IP: `, value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP}`},
+          )
     
         message.channel.send({embeds: [ip2embed]}) //.then(message => {setTimeout(() => message.delete(), 60000)});
-}}}
+   }) }) }}}
--- commands/ipprefixcommand.js
+++ commands/ipprefixcommand.js
@@ -1,17 +1,26 @@
 const prefix = '!'
+const list = require('minecraft-server-util');
+
 module.exports = {
     name: 'prefixip',
     execute(message,Discord,client){
       
       if(!message.content.startsWith(prefix) || message.author.bot) return;
       message.react('<:resolved:942896814154199150> ')
-    const ip2embed = new Discord.MessageEmbed()
+    
+      list.status('pandamium.eu').then((Responses) =>{
+      list.status('snapshot.pandamium.eu').then((Response) =>{
+        const snapVersionIP = Response.version.name
+        const releaseversionIP = Responses.version.name
+
+      const ip2embed = new Discord.MessageEmbed()
       .setColor('#008000')
       .setTitle("Pandamium Server IP's")
       .addFields(
-        {name:`Release IP:`,value:`pandamium.eu`},
-        {name: `Snapshot IP:`, value: `snapshot.pandamium.eu`}
+        {name:`Release IP: `,value:`pandamium.eu\n **Version:** ${releaseversionIP}\n\n`},
+        {name: `Snapshot IP: `, value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP}`},
         )
-    
+     
         message.channel.send({embeds: [ip2embed]}) //.then(message => {setTimeout(() => message.delete(), 60000)});
-}}
+
+      })     }) }}
