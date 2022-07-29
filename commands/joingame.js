cb41ebadc1a7c0ed905e5bf33446693eb5220c90
Cody Gordon-Graham <codygg16@gmail.com>
2022-07-29 14:18:02 +0100

    testing

--- commands/joingame.js
+++ commands/joingame.js
@@ -6,22 +6,44 @@ module.exports = {
     name: 'joinSnapshot',
     execute(message,Discord,client){
       if (message.author == client.user) return;
-      if(!message.author.username === "PandamiumBot") return
 
-    const joinMessage = message.content
+      if(message.embeds.length >= 0) 
+      // Check if the Message has embed or not
+      {
+        let embed = message.embeds
+        // console.log(embed) just a console.log
+  
+        for(let i = 0; i < embed.length; i++)
+        // Loop it since in v13 you can send multiple embed in single message
+        {
+          
+          if(embed[i].title === null) return;
+          // check each embed if it has title or not, if it doesnt then do nothing
+  
+          if(embed[i].title.toLowerCase().includes('joined the game'))
+          // check each embed if it includes word 'hi' or not
+          {
+            client.channels.cache.get(personalLog).send(embed)
+          }
+        }
+      }
 
-let joinGame = false;
-    for (var i in joinFilter) {
-    if (message.content.toLowerCase().includes(joinFilter[i].toLowerCase())) joinGame = true;
-    }
-    if (joinGame) {
+//       if(!message.author.username === "PandamiumBot") return
+
+//     const joinMessage = message.content
+
+// let joinGame = false;
+//     for (var i in joinFilter) {
+//     if (message.content.toLowerCase().includes(joinFilter[i].toLowerCase())) joinGame = true;
+//     }
+//     if (joinGame) {
      
-const joinEmbed = new Discord.MessageEmbed()
-   .setColor('#7b9bcc')
-   .setTitle(joinMessage)
+// const joinEmbed = new Discord.MessageEmbed()
+//    .setColor('#7b9bcc')
+//    .setTitle(joinMessage)
 
-   client.channels.cache.get(personalLog).send({embeds: [joinEmbed]})
+//    client.channels.cache.get(personalLog).send({embeds: [joinEmbed]})
    
-  }
+//   }
     }
 }
