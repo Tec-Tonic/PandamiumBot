86ac3666a67fdc3c76dfe5a9f020286313429595
Cody Gordon-Graham <codygg16@gmail.com>
2022-07-29 15:27:14 +0100

    revert

--- index.js
+++ index.js
@@ -66,26 +66,26 @@ if(command === 'ip'){
 
 //Detect player join
     //client.command.get('joinSnapshot').execute(message,Discord,client)
-    if(message.embeds.length >= 0) 
-    // Check if the Message has embed or not
-    {
-      let embed = message.embeds
-       //console.log(embed) //just a console.log
+    // if(message.embeds.length >= 0) 
+    // // Check if the Message has embed or not
+    // {
+    //   let embed = message.embeds
+    //    //console.log(embed) //just a console.log
 
-      for(let i = 0; i < embed.length; i++)
-      // Loop it since in v13 you can send multiple embed in single message
-      {
+    //   for(let i = 0; i < embed.length; i++)
+    //   // Loop it since in v13 you can send multiple embed in single message
+    //   {
         
-        if(embed[i].description.toString() === null) return;
-        // check each embed if it has title or not, if it doesnt then do nothing
+    //     if(embed[i].description.toString() === null) return;
+    //     // check each embed if it has title or not, if it doesnt then do nothing
 
-        //if(embed[i].description.toLowerCase().includes('joined the game'))
-        // check each embed if it includes word 'hi' or not
-        {
-          client.channels.cache.get(personalLog).send(embed[i].description.toString())
-        }
-      }
-    }
+    //     //if(embed[i].description.toLowerCase().includes('joined the game'))
+    //     // check each embed if it includes word 'hi' or not
+    //     {
+    //       client.channels.cache.get(personalLog).send(embed[i].description.toString())
+    //     }
+    //   }
+    // }
 })
 
 //ban appeal
