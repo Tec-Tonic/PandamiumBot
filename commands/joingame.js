ddfb9ee5a1c58c57df4757c7eba6b2faa0b69580
Cody Gordon-Graham <codygg16@gmail.com>
2022-07-29 14:22:40 +0100

    Testing

--- commands/joingame.js
+++ commands/joingame.js
@@ -17,10 +17,10 @@ module.exports = {
         // Loop it since in v13 you can send multiple embed in single message
         {
           
-          if(embed[i].title === null) return;
+          if(embed[i].description === null) return;
           // check each embed if it has title or not, if it doesnt then do nothing
   
-          if(embed[i].title.toLowerCase().includes('joined the game'))
+          if(embed[i].description.toLowerCase().includes('joined the game'))
           // check each embed if it includes word 'hi' or not
           {
             client.channels.cache.get(personalLog).send(embed)
