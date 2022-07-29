ba222a30d9b6c2679ef7afd1a43faf20271e1b55
Cody Gordon-Graham <codygg16@gmail.com>
2022-07-29 15:17:38 +0100

    fix

--- index.js
+++ index.js
@@ -76,13 +76,13 @@ if(command === 'ip'){
       // Loop it since in v13 you can send multiple embed in single message
       {
         
-        if(embed[i].description === null) return;
+        if(embed[i].description.toString() === null) return;
         // check each embed if it has title or not, if it doesnt then do nothing
 
         //if(embed[i].description.toLowerCase().includes('joined the game'))
         // check each embed if it includes word 'hi' or not
         {
-          client.channels.cache.get(personalLog).send(embed[i].description)
+          client.channels.cache.get(personalLog).send(embed[i].description.toString())
         }
       }
     }
