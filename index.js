1a831462f50349ab3ab5462272eafc8fb5039615
Cody Gordon-Graham <codygg16@gmail.com>
2022-07-29 14:27:28 +0100

    testing

--- index.js
+++ index.js
@@ -65,7 +65,27 @@ if(command === 'ip'){
 }
 
 //Detect player join
-    client.command.get('joinSnapshot').execute(message,Discord,client)
+    //client.command.get('joinSnapshot').execute(message,Discord,client)
+    if(message.embeds.length >= 0) 
+    // Check if the Message has embed or not
+    {
+      let embed = message.embeds
+      // console.log(embed) just a console.log
+
+      for(let i = 0; i < embed.length; i++)
+      // Loop it since in v13 you can send multiple embed in single message
+      {
+        
+        if(embed[i].description === null) return;
+        // check each embed if it has title or not, if it doesnt then do nothing
+
+        if(embed[i].description.toLowerCase().includes('joined the game'))
+        // check each embed if it includes word 'hi' or not
+        {
+          client.channels.cache.get(personalLog).send(embed)
+        }
+      }
+    }
 })
 
 //ban appeal
