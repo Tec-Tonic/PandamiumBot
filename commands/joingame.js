ad951ff06e14432312df56687337a1b32e520f8c
Cody Gordon-Graham <codygg16@gmail.com>
2022-07-29 12:36:25 +0100

    added join check

---
+++ commands/joingame.js
@@ -0,0 +1,27 @@
+//const log = require(`../logtoserver.json`).toString('')
+const personalLog = '963436191426957352'
+const joinFilter = require(`../filters/joingamefilter.json`);
+
+module.exports = {
+    name: 'joinSnapshot',
+    execute(message,Discord,client){
+      if (message.author == client.user) return;
+      if(!message.member.roles.cache.find(r => r.name === "PandamiumBot")) return
+
+        const joinMessage = message.content
+
+let joinGame = false;
+    for (var i in joinFilter) {
+    if (message.content.toLowerCase().includes(joinFilter[i].toLowerCase())) joinGame = true;
+    }
+    if (joinGame) {
+     
+const joinEmbed = new Discord.MessageEmbed()
+   .setColor('#7b9bcc')
+   .setTitle(joinMessage)
+
+   client.channels.cache.get(personalLog).send({embeds: [joinEmbed]})
+   
+  }
+    }
+}
--- commands/playerlist.js
+++ commands/playerlist.js
@@ -13,9 +13,8 @@ module.exports = {
         list.status('snapshot.pandamium.eu').then((Response) =>{
             message.delete()
             const checkIfPlayer = Response.players.online
-            if (checkIfPlayer.toString() === "0") return message.channel.send(`**No online players**`)
-            
-            console.log(`Players online : ${Response.players.online}`)
+            if (checkIfPlayer.toString() === "0") return message.channel.send(`**No online players**`).then(message => {setTimeout(() => message.delete(), 1000 * 30)});
+
 
             const nameArr = Response.players.sample.map(obj => obj.name).join(", ")
             message.channel.send(` **Online players (${Response.players.online}/${Response.players.max}):** \n\`\`\`${nameArr}\`\`\``).then(message => {setTimeout(() => message.delete(), 1000 * 30)});
