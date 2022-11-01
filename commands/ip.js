4de86c9aaf2ae0dd3e1b3756e93388e19575ec60
Cody Gordon-Graham <codygg16@gmail.com>
2022-11-01 14:01:54 +0000

    updated scams

--- commands/ip.js
+++ commands/ip.js
@@ -33,7 +33,18 @@ module.exports = class IpSlashCommand extends BaseSlashCommand {
             }
           );
 
-        return interaction.reply({ embeds: [ipEmbed], ephemeral: true });
+          const CopyPasteIP = new ActionRowBuilder().addComponents(
+                  new ButtonBuilder()
+                    .setLabel("Release IP")
+                    .setStyle(ButtonStyle.Link)
+                    .setURL("https://pastebin.com/raw/LyNSkBrS"),
+                  new ButtonBuilder()
+                    .setLabel("Snapshot IP")
+                    .setStyle(ButtonStyle.Link)
+                    .setURL("https://minecraftservers.org/vote/562059"),
+          )
+
+        return interaction.reply({ embeds: [ipEmbed], components: [CopyPasteIP] ,ephemeral: true });
       });
     });
   }
@@ -45,3 +56,51 @@ module.exports = class IpSlashCommand extends BaseSlashCommand {
       .toJSON();
   }
 };
+
+// const BaseSlashCommand = require("../utils/BaseSlashCommands");
+// const {
+//   SlashCommandBuilder,
+//   ButtonStyle,
+//   ActionRowBuilder,
+//   ButtonBuilder,
+//   EmbedBuilder,
+// } = require("discord.js");
+// const util = require("minecraft-server-util");
+
+// module.exports = class IpSlashCommand extends BaseSlashCommand {
+//   constructor() {
+//     super("ip");
+//   }
+
+//   run(client, interaction) {
+//     util.status("pandamium.eu").then((ResponseRelease) => {
+//       util.status("snapshot.pandamium.eu").then((ResponseSnapshot) => {
+//         const releaseVersionIP = ResponseRelease.version.name;
+//         const snapVersionIP = ResponseSnapshot.version.name;
+
+//         const ipEmbed = new EmbedBuilder()
+//           .setColor("#008000")
+//           .setTitle("Pandamium Server IP's")
+//           .addFields(
+//             {
+//               name: `Release IP: `,
+//               value: `pandamium.eu\n **Version:** ${releaseVersionIP}\n\n`,
+//             },
+//             {
+//               name: `Snapshot IP: `,
+//               value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP}`,
+//             }
+//           );
+
+//         return interaction.reply({ embeds: [ipEmbed], ephemeral: true });
+//       });
+//     });
+//   }
+
+//   getSlashCommandJSON() {
+//     return new SlashCommandBuilder()
+//       .setName(this.name)
+//       .setDescription("Replies with the server(s) IP")
+//       .toJSON();
+//   }
+// };
