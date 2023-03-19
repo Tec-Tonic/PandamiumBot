76bc65315ceb5e75a7528211f898a9c60f1cf815
Cody Gordon-Graham <codygg16@gmail.com>
2023-03-19 23:34:50 +0000

    added link quotes

---
+++ commands/quote.js
@@ -0,0 +1,53 @@
+const BaseSlashCommand = require("../utils/BaseSlashCommands");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  discordSort,
+  DiscordAPIError,
+  Discord,
+} = require("discord.js");
+
+module.exports = class QuoteSlashCommand extends BaseSlashCommand {
+  constructor() {
+    super("quote");
+  }
+
+  async run(client, interaction) {
+    const link = interaction.options.getString('link')
+    const ErrlinkReply = new EmbedBuilder().setDescription(`I do not have access to this message, provide a message link from this server.`).setColor('#FF0000')
+
+const discordLinkReg = /\/([0-9].*)\/([0-9].*)\/([0-9].*[^/])\/{0,}/
+const parsed = discordLinkReg.exec(link)
+const messageId = parsed[3];
+const channelId = parsed[2];
+const guildId = parsed[1];
+
+console.log(guildId)
+
+if(guildId === '797494884159848469') {
+
+    const channel = await client.channels.fetch(channelId);
+    const message = await channel.messages.fetch(messageId);
+
+    const linkReply = new EmbedBuilder().setAuthor({ name: `${message.author.tag}`, iconURL: `https://cdn.discordapp.com/avatars/`+message.author.id+`/`+message.author.avatar+`.jpeg`, url: 'https://www.pandamium.com' }).setDescription(`${message.content}`).setColor('#1EE3CE')
+    
+    await interaction.reply({ embeds: [linkReply]});
+} else {
+    interaction.reply({ embeds: [ErrlinkReply]})
+}
+
+
+
+  }
+
+  getSlashCommandJSON() {
+    return new SlashCommandBuilder()
+      .setName(this.name)
+      .setDescription("Converts a Discord message link into an Embed! [Visible to everyone]")
+      .addStringOption(option =>
+        option.setName('link')
+            .setDescription('link required')
+            .setRequired(true))
+      .toJSON();
+  }
+};
