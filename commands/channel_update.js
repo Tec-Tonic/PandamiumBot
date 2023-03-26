92c83b62cb9a7c20a5fb981284f4b569631d8b85
Cody Gordon-Graham <codygg16@gmail.com>
2023-03-26 02:10:20 +0100

    update

--- commands/channel_update.js
+++ commands/channel_update.js
@@ -97,7 +97,73 @@ https://minecraft.global/server/173/vote
    await interaction.channel.send({embeds: [embed1]})
         }
         if (update === "faq") {
-            return;
+            const f1 = new EmbedBuilder()
+.setDescription(`
+**‣ What's the IP of the server?**
+  Release server: release.pandamium.eu
+  Snapshot server: snapshot.pandamium.eu
+`).setColor('#FFA200')
+
+const f2 = new EmbedBuilder()
+.setDescription(`
+**‣ When do auto restarts happen?**
+  Snapshot server: Every 12 hours - 1:00 and 13:00 (1am/pm GMT)
+  Release server: Every 12 hours - 12:00 and 00:00 (12am/pm GMT)
+`).setColor('#FFA200')
+
+const f3 = new EmbedBuilder()
+.setDescription(`
+**‣ What hardware is the server running on?**
+  The hardware we're running is basically ideal for a Minecraft server, we have a CPU with great single core performance, more cores and RAM than the server actually needs and a fast NVMe SSD. If the server is lagging, it's because Minecraft is poorly optimized, not because of the server hardware.
+
+:white_small_square:AMD Ryzen 3600 (6C/12T @3.6Ghz)
+:white_small_square:64 GB DDR4 ECC RAM, each of the two servers has 16GB allocated.
+:white_small_square:2x 500gb NVMe SSDs running in RAID1
+:white_small_square:2x 1tb NVMe SSDs running in RAID1 for server backup.
+:white_small_square:1 GBit Internet connection
+`).setColor('#FFA200')
+
+const f4 = new EmbedBuilder()
+.setDescription(`
+**‣ Is griefing and stealing allowed?**
+  No, it's absolutely not allowed. On the Release server you can create a claim to protect your stuff. Unfortunately this isn't possible on the Snapshot server because we can't use plugins there, so please move far away from spawn and only share the coords with people you trust. 
+ 
+`).setColor('#FFA200')
+
+const f5 = new EmbedBuilder()
+.setDescription(`
+**‣ How can I get more homes or bigger claims?**
+  You can rank up by reaching a certain amount of playtime and votes. For more info, check out <#1089274345702039673> Instead you can also donate if you want, check out \n<#1089274399200391178>.
+ 
+`).setColor('#FFA200')
+
+const f6 = new EmbedBuilder()
+.setDescription(`
+**‣ How can I join or create a town?**
+To join a town, you need to apply in their Discord channel. It is up to the owners of the respective groups to accept/deny your application. To create your own, you need to open a ticket in <#1089274100565950596> and follow the correct template for the town application.
+ 
+`).setColor('#FFA200')
+
+const f7 = new EmbedBuilder()
+.setDescription(`
+**‣ Is it allowed to use Litematica to make building easier?**
+  You are allowed to use the visual features of Litematica. However, Litematica allows you to place blocks in the air or other impossible positions and allows automates building to make if much faster than for normal players. These parts of the mod are **not **allowed.
+
+`).setColor('#FFA200')
+
+const f8 = new EmbedBuilder()
+.setDescription(`
+**‣ How are chunks reset?**
+*Note: This does not refer to world resets but to periodic trimming of the world size!*
+
+  Exploring freely on Pandamium causes lots of chunks to be loaded (e.g. while flying with an elytra) and then never even loaded again. Those chunks take up a lot of space, so we need to trim the world size from time to time. For this we use a tool that only deletes chunks which haven't been loaded at all in at least two weeks and for more than 2 hours in total. World trimming isn't done regularly, we usually do it together with other server maintenance since the server has to be offline for it.
+
+`).setColor('#FFA200')
+
+
+
+await interaction.channel.send('https://cdn.discordapp.com/attachments/1079531915155079178/1089235100606156941/FAQ.png')
+await interaction.channel.send({embeds: [f1, f2, f3, f4, f5, f6, f7, f8]})
         }
         if (update === "rank") {
             const embed = new EmbedBuilder().setTitle(`GUEST`)
@@ -105,97 +171,101 @@ https://minecraft.global/server/173/vote
     New playersㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     
     **Snapshot Perks:**
-    Homes: 1
+    **▫️**Homes: 1
     
     **Release Perks:**
-    Homes: **2**
-    Claims: **1**
-    Max Claim Area: **5,000 Blocks**`).setColor('#AAAAAA')
+    **▫️**Homes: **2**
+    **▫️**Claims: **1**
+    **▫️**Max Claim Area: **5,000 Blocks**`).setColor('#AAAAAA')
     
             const embed2 = new EmbedBuilder().setTitle(`PLAYER`)
             .setDescription(`
     Requires 5 hours of playtime and 5 votes.
      
     **Snapshot Perks:**
-    Homes: **2 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Homes: **2 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
             
     **Release Perks: **
-    Homes: **3**
-    Claims: **2**
-    Max Claim Area: **10,000 Blocks **`).setColor('#55FF55')
+    **▫️**Homes: **3**
+    **▫️**Claims: **2**
+    **▫️**Max Claim Area: **10,000 Blocks **`).setColor('#55FF55')
            
             
             const embed3 = new EmbedBuilder().setTitle(`MEMBER`)
             .setDescription(`
-            Requires 25 hours of playtime and 25 votes.
+    Requires 25 hours of playtime and 25 votes.
      
     **Snapshot Perks:**
-    Homes: **3 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Homes: **3 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     
     **Release Perks: **
-    Homes: **5**
-    Claims: **3**
-    Max Claim Area: **25,000 Blocks **`).setColor('#00AA00')
+    **▫️**Homes: **5**
+    **▫️**Claims: **3**
+    **▫️**Max Claim Area: **25,000 Blocks **`).setColor('#00AA00')
     
             const embed4 = new EmbedBuilder().setTitle(`ELDER`)
             .setDescription(`
-            Requires 125 hours of playtime and 125 votes.
+    Requires 125 hours of playtime and 125 votes.
      
     **Snapshot Perks:**
-    Homes: **4 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Homes: **4 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     
     **Release Perks: **
-    Homes: **10**
-    Claims: **5**
-    Max Claim Area: **50,000 Blocks **`).setColor('#55FFFF')
+    **▫️**Homes: **10**
+    **▫️**Claims: **5**
+    **▫️**Max Claim Area: **50,000 Blocks **`).setColor('#55FFFF')
     
             const embed5 = new EmbedBuilder().setTitle(`VETERAN`)
             .setDescription(`
-            Requires 500 hours of playtime and 500 votes.
+    Requires 500 hours of playtime and 500 votes.
      
     **Snapshot Perks:**
-    Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     
     **Release Perks: **
-    Homes: **20**
-    Claims: **10**
-    Max Claim Area: **150,000 Blocks **`).setColor('#00AAAA')
+    **▫️**Homes: **20**
+    **▫️**Claims: **10**
+    **▫️**Max Claim Area: **150,000 Blocks **`).setColor('#00AAAA')
     
             const embed6 = new EmbedBuilder().setTitle(`ELITE`)
             .setDescription(`
-            Requires 2500 hours of playtime and 2500 votes.
+    Requires 2500 hours of playtime and 2500 votes.
      
     **Snapshot Perks:**
-    Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     
     **Release Perks: **
-    Homes: **20**
-    Claims: **10**
-    Max Claim Area: **150,000 Blocks **`).setColor('#5555FF')
+    **▫️**Homes: **20**
+    **▫️**Claims: **10**
+    **▫️**Max Claim Area: **150,000 Blocks **`).setColor('#5555FF')
     
             const embed7 = new EmbedBuilder().setTitle(`DONATOR`)
             .setDescription(`
-            Players who have donated.
-     
-    **Snapshot Perks:**
-    Homes: **10**ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
-    
-    **Release Perks: **
-    Homes: **25**
-    Claims: **15**
-    Max Claim Area: **250,000 Blocks **`).setColor('#AA00AA')
+    Players who have donated.
+             
+    **Snapshot Perks**:
+    **▫️**Homes: 10ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Access to lots of particle effects
+    **▫️**Donator only <#1089274505295314964> 
+            
+    **Release Perks**: 
+    **▫️**Homes: 25
+    **▫️**Claims: 15
+    **▫️**Max Claim Area: 250,000 Blocks
+    **▫️**Join even if the server is full
+    **▫️**Donator only <#1089274505295314964>`).setColor('#AA00AA')
     
             const embed8 = new EmbedBuilder().setTitle(`VIP`)
             .setDescription(`
     Not obtainable via normal means and only given by the Owner. 
      
     **Snapshot Perks:**
-    Homes: **10**ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+    **▫️**Homes: **10**ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
             
     **Release Perks**
-    Homes: **25**
-    Claims: **15**
-    Max Claim Area: **250,000 Blocks**`).setColor('#0000AA')
+    **▫️**Homes: **25**
+    **▫️**Claims: **15**
+    **▫️**Max Claim Area: **250,000 Blocks**`).setColor('#0000AA')
     
     const embed9 = new EmbedBuilder().setTitle('HELPER')
     .setDescription(`
@@ -209,18 +279,19 @@ https://minecraft.global/server/173/vote
     
     const embed11 = new EmbedBuilder().setTitle('SR. MODERATOR')
     .setDescription(`
-    Moderators who are really active and dedicated to the server and do an extremely good job for a long time might be promoted to Sr. Moderator.
+    Sr. Moderators have the highest moderation staff permissionsㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     `).setColor('#ffaa00')
     
     const embed12 = new EmbedBuilder().setTitle('ADMIN')
     .setDescription(`
-    The highest obtainable staff rank, only the most reliable staff members who, over a very long period of time, have proven again and again that they're doing an awesome job might get become Admins.
+    The highest obtainable staff rank, only those that are already Sr. Moderators and would benefit from access to the console may become Admin.
     `).setColor('#FF5555')
     
     const embed13 = new EmbedBuilder().setTitle('OWNER')
     .setDescription(`
     Owns the Discord and Minecraft servers.ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
     `).setColor('#AA0000')
+    
     
             await interaction.channel.send('https://cdn.discordapp.com/attachments/1089274345702039673/1089325323226718220/player_ranks.png')
             await interaction.channel.send({embeds: [embed, embed2 , embed3 , embed4, embed5, embed6, embed7, embed8]})
@@ -256,8 +327,8 @@ https://minecraft.global/server/173/vote
     
     await interaction.channel.send('https://cdn.discordapp.com/attachments/1079532215584706670/1089244539597897780/donations.png')
      await interaction.channel.send(`
-     If you would like to support Pandamium and help improve the hardware the server is running on, consider donating. 
-     In return, you'll receive the **Donator** rank as a thank you, both in Minecraft and on Discord. `)
+If you would like to support Pandamium and help improve the hardware the server is running on, consider donating.
+In return, you'll receive the **Donator** rank as a thank you, both in Minecraft and on Discord. `)
     
      await interaction.channel.send({embeds: [emb]})
      await interaction.channel.send({embeds: [emb2]})
