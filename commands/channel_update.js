fe5ccb3de0d67acbba45bf5372e10667c9e3ccf6
Cody Gordon-Graham <codygg16@gmail.com>
2023-03-27 20:08:29 +0100

    forgot one

--- commands/channel_update.js
+++ commands/channel_update.js
@@ -577,8 +577,8 @@ You can view all of the mob heads on our [**website**](https://www.pandamium.com
 If you want to apply for a town Discord channel, read the following carefully.
 
 **TOWN REQUIREMENTS**
-↳ The town must have at least six members to be considered for acceptance. 
-↳ You must provide screenshots of the town/area where it’s planned to be built.
+**↳** The town must have at least six members to be considered for acceptance. 
+**↳** You must provide screenshots of the town/area where it’s planned to be built.
 
 **HOW TO APPLY**
 :white_small_square:Open a ticket in <#1089326603672223794> and follow the directions.
