c1cad13e1c676c1ab761eab9e13064c8bc07b691
Cody Gordon-Graham <codygg16@gmail.com>
2023-03-30 16:38:22 +0100

    fixed wrong var input

--- embeds/reaction-role-embed.js
+++ embeds/reaction-role-embed.js
@@ -16,7 +16,7 @@ React below to select which pings you would like to receive:
 :mega: **⭢ Snapshot Announcement Pings**
 <:snapshotchangelogping:1089615059090677760>  **⭢ Snapshot Changelog Pings**
 <:releaseannouncementping:1089613992491753573>  **⭢ Release Announcement Pings**
-<:snapshotchangelogping:1089615059090677760> **⭢ Release Changelog Pings**
+<:releasechangelogping:1089615057324888235>  **⭢ Release Changelog Pings**
 <:eyeofender:1089616416929489047>  **⭢ End Reset Pings**
 :tada: **⭢ Event Pings**
 `).setColor('#F53068')
