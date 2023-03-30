4e167d6189d275ad93802a29e409b436bc369af2
Cody Gordon-Graham <codygg16@gmail.com>
2023-03-30 16:35:27 +0100

    fixed emoji ID

--- embeds/reaction-role-embed.js
+++ embeds/reaction-role-embed.js
@@ -14,10 +14,10 @@ const {
 React below to select which pings you would like to receive:
 
 :mega: **⭢ Snapshot Announcement Pings**
-:snapshotchangelogping: **⭢ Snapshot Changelog Pings**
-:releaseannouncementping: **⭢ Release Announcement Pings**
-:releasechangelogping: **⭢ Release Changelog Pings**
-:eyeofender: **⭢ End Reset Pings**
+<:snapshotchangelogping:1089615059090677760>  **⭢ Snapshot Changelog Pings**
+<:releaseannouncementping:1089613992491753573>  **⭢ Release Announcement Pings**
+<:snapshotchangelogping:1089615059090677760> **⭢ Release Changelog Pings**
+<:eyeofender:1089616416929489047>  **⭢ End Reset Pings**
 :tada: **⭢ Event Pings**
 `).setColor('#F53068')
 
