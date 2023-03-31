19250502066ae40b5dc0c863f30ae9e554199f91
Cody Gordon-Graham <codygg16@gmail.com>
2023-03-31 18:53:42 +0100

    fixed arrows for mobile

--- embeds/reaction-role-embed.js
+++ embeds/reaction-role-embed.js
@@ -11,7 +11,7 @@ const {
   
   const rr = new EmbedBuilder()
 .setDescription(`
-React below to select which pings you would like to receive:\n\n:mega: **⭢ Snapshot Announcement Pings**\n<:snapshotchangelogping:1089615059090677760>  **⭢ Snapshot Changelog Pings**\n<:releaseannouncementping:1089613992491753573>  **⭢ Release Announcement Pings**\n<:releasechangelogping:1089615057324888235>  **⭢ Release Changelog Pings**\n<:eyeofender:1089616416929489047>  **⭢ End Reset Pings**\n:tada: **⭢ Event Pings**
+React below to select which pings you would like to receive:\n\n:mega:  **-> Snapshot Announcement Pings**\n<:snapshotchangelogping:1089615059090677760>  **-> Snapshot Changelog Pings**\n<:releaseannouncementping:1089613992491753573>  **-> Release Announcement Pings**\n<:releasechangelogping:1089615057324888235>  **-> Release Changelog Pings**\n<:eyeofender:1089616416929489047>  **-> End Reset Pings**\n:tada:  **-> Event Pings**
 `).setColor('#F53068')
 
 module.exports.rr = rr
