c3153a55ee4e6cc0c8fd6ff0515837673dcd06f3
Cody Gordon-Graham <codygg16@gmail.com>
2023-04-25 13:41:15 +0100

    update

--- commands/edit.js
+++ commands/edit.js
@@ -45,8 +45,8 @@ module.exports = class faqSlashCommand extends BaseSlashCommand {
         const noUpdates = new EmbedBuilder().setDescription('There is nothing to update!').setColor('#189cab')
         //interaction.reply({embeds: [noUpdates], ephemeral: true})
         
-        const msgID = '1091447597224448081'
-        const channelID = '506987588742152202'
+        const msgID = '1091447597224448081' 
+        const channelID = '506987588742152202' 
 
         const channel = client.channels.cache.get(channelID) //commands
         channel.messages.fetch(msgID).then(msg => msg.edit({embeds: [
--- embeds/rank-embed.js
+++ embeds/rank-embed.js
@@ -28,11 +28,11 @@ Requires 125 hours of playtime and 125 votes.\n\n**Snapshot Perks:**\n**▫️**
 
         const embed5 = new EmbedBuilder().setTitle(`VETERAN`)
         .setDescription(`
-Requires 500 hours of playtime and 500 votes.\n\n**Snapshot Perks:**\n**▫️**Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n\n**Release Perks: **\n**▫️**Homes: **20**\n**▫️**Claims: **10**\n**▫️**Max Claim Area: **150,000 Blocks **`).setColor('#00AAAA')
+Requires 500 hours of playtime and 500 votes.\n\n**Snapshot Perks:**\n**▫️**Homes: **5 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n\n**Release Perks: **\n**▫️**Homes: **15**\n**▫️**Claims: **10**\n**▫️**Max Claim Area: **150,000 Blocks **`).setColor('#00AAAA')
 
         const embed6 = new EmbedBuilder().setTitle(`ELITE`)
         .setDescription(`
-Requires 2500 hours of playtime and 2500 votes.\n\n**Snapshot Perks:**\n**▫️**Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n\n**Release Perks: **\n**▫️**Homes: **25**\n**▫️**Claims: **15**\n**▫️**Max Claim Area: **250,000 Blocks **`).setColor('#5555FF')
+Requires 2500 hours of playtime and 2500 votes.\n\n**Snapshot Perks:**\n**▫️**Homes: **7 **ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n\n**Release Perks: **\n**▫️**Homes: **20**\n**▫️**Claims: **15**\n**▫️**Max Claim Area: **250,000 Blocks **`).setColor('#5555FF')
 
         const embed7 = new EmbedBuilder().setTitle(`DONATOR`)
         .setDescription(`
