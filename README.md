ca07846ddabbfd323fcd6d1fce172ee6322a155d
Cody Gordon-Graham <codygg16@gmail.com>
2023-05-02 00:58:08 +0100

    fixed GuildMemberAdd crash

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Utility
+# PandamiumBot Jr.
 - Basic moderation and Utility bot, hard coded for [Pandamium Discord server](http://discord.pandamium.eu/)
--- src/bot.js
+++ src/bot.js
@@ -80,25 +80,27 @@ client.on("messageCreate", async (message) => {
 // welcome reactions
 // TODO: Add Welcome channel ID to .env and Heroku config
 //       Add a useful User Embed (info about them ect..)
+//       Move to src/events/client -> Will allow for a cleaners bot.js file
 client.on("guildMemberAdd", (user) => {
-  const pandaEmoji = '👍'//`<:pandamium:797762197567832105>`;
+  const pandaEmoji = `<:pandamium:797762197567832105>`;
   setTimeout(() => {
     const message =
       user.guild.channels.cache.get(welcome).lastMessage;
     message.react(pandaEmoji);
   }, 500);
 
-  const authorEmbed = new EmbedBuilder()
-    .setColor("#FF0000")
-    .setThumbnail(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.jpeg`)
-    .setDescription(
-      `**<@${user.id}> joined**
+  // Update this
+  // const authorEmbed = new EmbedBuilder()
+  //   .setColor("#FF0000")
+  //   .setThumbnail(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.jpeg`)
+  //   .setDescription(
+  //     `**<@${user.id}> joined**
 
-      ID: ${user.id} \nJoined Discord: ${new Date(
-        user.createdTimestamp
-      ).toLocaleDateString()}`
-    );
-    client.channels.cache.get(logs).send({ embeds: [authorEmbed] })
+  //     ID: ${user.id} \nJoined Discord: ${new Date(
+  //       user.createdTimestamp
+  //     ).toLocaleDateString()}`
+  //   );
+  //   client.channels.cache.get(logs).send({ embeds: [authorEmbed] })
 });
 
 client.eventHandler();
