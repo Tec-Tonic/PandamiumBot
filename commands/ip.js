2dff80efb4b5cff22062aaaf5c5e172a358ae259
Cody Gordon-Graham <codygg16@gmail.com>
2023-05-01 22:15:44 +0100

    delete

--- commands/ip.js
+++ commands/ip.js
@@ -1,62 +1,3 @@
-const BaseSlashCommand = require("../utils/BaseSlashCommands");
-const {
-  SlashCommandBuilder,
-  ButtonStyle,
-  ActionRowBuilder,
-  ButtonBuilder,
-  EmbedBuilder,
-} = require("discord.js");
-const util = require("minecraft-server-util");
-
-module.exports = class IpSlashCommand extends BaseSlashCommand {
-  constructor() {
-    super("ip");
-  }
-
-  run(client, interaction) {
-    util.status("pandamium.eu").then((ResponseRelease) => {
-      util.status("snapshot.pandamium.eu").then((ResponseSnapshot) => {
-        const releaseVersionIP = ResponseRelease.version.name;
-        const snapVersionIP = ResponseSnapshot.version.name;
-
-        const ipEmbed = new EmbedBuilder()
-          .setColor("#008000")
-          .setTitle("Pandamium Server IP's")
-          .addFields(
-            {
-              name: `Release IP: `,
-              value: `release.pandamium.eu\n **Version:** ${releaseVersionIP}\n\n`,
-            },
-            {
-              name: `Snapshot IP: `,
-              value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP} \n\nⓘ | Buttons below will take you to [pastebin](https://pastebin.com/)`,
-            }
-          )
-
-          const CopyPasteIP = new ActionRowBuilder().addComponents(
-                  new ButtonBuilder()
-                    .setLabel("Release IP")
-                    .setStyle(ButtonStyle.Link)
-                    .setURL("https://pastebin.com/raw/cfi77L2e"),
-                  new ButtonBuilder()
-                    .setLabel("Snapshot IP")
-                    .setStyle(ButtonStyle.Link)
-                    .setURL("https://pastebin.com/raw/LyNSkBrS"),
-          )
-
-        return interaction.reply({ embeds: [ipEmbed],components: [CopyPasteIP],ephemeral: true });
-      });
-    });
-  }
-
-  getSlashCommandJSON() {
-    return new SlashCommandBuilder()
-      .setName(this.name)
-      .setDescription("Replies with the server(s) IP")
-      .toJSON();
-  }
-};
-
 // const BaseSlashCommand = require("../utils/BaseSlashCommands");
 // const {
 //   SlashCommandBuilder,
@@ -84,15 +25,26 @@ module.exports = class IpSlashCommand extends BaseSlashCommand {
 //           .addFields(
 //             {
 //               name: `Release IP: `,
-//               value: `pandamium.eu\n **Version:** ${releaseVersionIP}\n\n`,
+//               value: `release.pandamium.eu\n **Version:** ${releaseVersionIP}\n\n`,
 //             },
 //             {
 //               name: `Snapshot IP: `,
-//               value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP}`,
+//               value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP} \n\nⓘ | Buttons below will take you to [pastebin](https://pastebin.com/)`,
 //             }
-//           );
+//           )
 
-//         return interaction.reply({ embeds: [ipEmbed], ephemeral: true });
+//           const CopyPasteIP = new ActionRowBuilder().addComponents(
+//                   new ButtonBuilder()
+//                     .setLabel("Release IP")
+//                     .setStyle(ButtonStyle.Link)
+//                     .setURL("https://pastebin.com/raw/cfi77L2e"),
+//                   new ButtonBuilder()
+//                     .setLabel("Snapshot IP")
+//                     .setStyle(ButtonStyle.Link)
+//                     .setURL("https://pastebin.com/raw/LyNSkBrS"),
+//           )
+
+//         return interaction.reply({ embeds: [ipEmbed],components: [CopyPasteIP],ephemeral: true });
 //       });
 //     });
 //   }
@@ -104,3 +56,51 @@ module.exports = class IpSlashCommand extends BaseSlashCommand {
 //       .toJSON();
 //   }
 // };
+
+// // const BaseSlashCommand = require("../utils/BaseSlashCommands");
+// // const {
+// //   SlashCommandBuilder,
+// //   ButtonStyle,
+// //   ActionRowBuilder,
+// //   ButtonBuilder,
+// //   EmbedBuilder,
+// // } = require("discord.js");
+// // const util = require("minecraft-server-util");
+
+// // module.exports = class IpSlashCommand extends BaseSlashCommand {
+// //   constructor() {
+// //     super("ip");
+// //   }
+
+// //   run(client, interaction) {
+// //     util.status("pandamium.eu").then((ResponseRelease) => {
+// //       util.status("snapshot.pandamium.eu").then((ResponseSnapshot) => {
+// //         const releaseVersionIP = ResponseRelease.version.name;
+// //         const snapVersionIP = ResponseSnapshot.version.name;
+
+// //         const ipEmbed = new EmbedBuilder()
+// //           .setColor("#008000")
+// //           .setTitle("Pandamium Server IP's")
+// //           .addFields(
+// //             {
+// //               name: `Release IP: `,
+// //               value: `pandamium.eu\n **Version:** ${releaseVersionIP}\n\n`,
+// //             },
+// //             {
+// //               name: `Snapshot IP: `,
+// //               value: `snapshot.pandamium.eu\n **Version:** ${snapVersionIP}`,
+// //             }
+// //           );
+
+// //         return interaction.reply({ embeds: [ipEmbed], ephemeral: true });
+// //       });
+// //     });
+// //   }
+
+// //   getSlashCommandJSON() {
+// //     return new SlashCommandBuilder()
+// //       .setName(this.name)
+// //       .setDescription("Replies with the server(s) IP")
+// //       .toJSON();
+// //   }
+// // };
--- commands/translator.js
+++ commands/translator.js
@@ -1,57 +1,57 @@
-const {
-    Client,
-    ContextMenuCommandInteraction,
-    ApplicationCommandType,
-  } = require("discord.js");
-  const BaseSlashCommand = require("../utils/BaseSlashCommands");
-  const {
-    SlashCommandBuilder,
-    EmbedBuilder,
-    ContextMenuCommandBuilder,
-    ActionRowBuilder,
-    StringSelectMenuBuilder,
-  } = require("discord.js");
-  const translate = require("@iamtraction/google-translate");
-  const ISO6391 = require("iso-639-1");
-  const sendToConsole = "1037707228897095811"; //jarvisconsole
+// const {
+//     Client,
+//     ContextMenuCommandInteraction,
+//     ApplicationCommandType,
+//   } = require("discord.js");
+//   const BaseSlashCommand = require("../utils/BaseSlashCommands");
+//   const {
+//     SlashCommandBuilder,
+//     EmbedBuilder,
+//     ContextMenuCommandBuilder,
+//     ActionRowBuilder,
+//     StringSelectMenuBuilder,
+//   } = require("discord.js");
+//   const translate = require("@iamtraction/google-translate");
+//   const ISO6391 = require("iso-639-1");
+//   const sendToConsole = "1037707228897095811"; //jarvisconsole
   
-  module.exports = class TranslateCommand extends BaseSlashCommand {
-    constructor() {
-      super("translate");
-    }
+//   module.exports = class TranslateCommand extends BaseSlashCommand {
+//     constructor() {
+//       super("translate");
+//     }
   
-    async run(client, interaction) {
+//     async run(client, interaction) {
       
-    const ERRembed = new EmbedBuilder().setColor("#FF0000").setTitle(`Unable to translate!`);
+//     const ERRembed = new EmbedBuilder().setColor("#FF0000").setTitle(`Unable to translate!`);
 
-    const foreignLanguage = interaction.options.getString("input");
+//     const foreignLanguage = interaction.options.getString("input");
 
-    translate(foreignLanguage, { to: `en` })
-      .then((res) => {
-        const getIsoName = res.from.language.iso;
-        const isoName = ISO6391.getName(getIsoName);
+//     translate(foreignLanguage, { to: `en` })
+//       .then((res) => {
+//         const getIsoName = res.from.language.iso;
+//         const isoName = ISO6391.getName(getIsoName);
 
-        const translatedEmbed = new EmbedBuilder().setColor("#00FFFF").setDescription(`${isoName} -> English`).setFields({ name: `Original Message :`, value: `${foreignLanguage}` }, { name: `Translation :`, value: `${res.text}` }).setFooter({ text: "Google Translate", iconURL: "https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png", });
-        interaction.reply({ embeds: [translatedEmbed], ephemeral: true });
-      })
-      .catch((err) => {
-        interaction.reply({ embeds: [ERRembed], ephemeral: true });
-        console.error(err);
-      });
+//         const translatedEmbed = new EmbedBuilder().setColor("#00FFFF").setDescription(`${isoName} -> English`).setFields({ name: `Original Message :`, value: `${foreignLanguage}` }, { name: `Translation :`, value: `${res.text}` }).setFooter({ text: "Google Translate", iconURL: "https://www.transparentpng.com/thumb/google-logo/google-logo-png-icon-free-download-SUF63j.png", });
+//         interaction.reply({ embeds: [translatedEmbed], ephemeral: true });
+//       })
+//       .catch((err) => {
+//         interaction.reply({ embeds: [ERRembed], ephemeral: true });
+//         console.error(err);
+//       });
 
-  }
+//   }
       
       
       
-  getSlashCommandJSON() {
-    return new SlashCommandBuilder()
-        .setName(this.name)
-        .setDescription("Translates text to English")
-        .addStringOption(option =>
-            option.setName('input')
-                .setDescription('text required')
-                .setRequired(true))
-        .toJSON();
-}
-};
+//   getSlashCommandJSON() {
+//     return new SlashCommandBuilder()
+//         .setName(this.name)
+//         .setDescription("Translates text to English")
+//         .addStringOption(option =>
+//             option.setName('input')
+//                 .setDescription('text required')
+//                 .setRequired(true))
+//         .toJSON();
+// }
+// };
   
