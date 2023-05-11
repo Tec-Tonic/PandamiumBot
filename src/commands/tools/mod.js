5f80afba49e0ae36d800468d3a74fd1999ad143f
Cody Gordon-Graham <codygg16@gmail.com>
2023-05-11 08:57:52 +0100

    updating mod command

--- src/commands/tools/mod.js
+++ src/commands/tools/mod.js
@@ -1,41 +1,56 @@
 // TODO: Improve this file, but for now use this.
 
 const {
-    SlashCommandBuilder,
-    ButtonStyle,
-    ActionRowBuilder,
-    ButtonBuilder,
-    EmbedBuilder,
-    PermissionFlagsBits,
-    AttachmentBuilder,
-  } = require("discord.js");
+  SlashCommandBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ButtonBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+  AttachmentBuilder,
+} = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('mod')
-    .setDescription('Gets current mod embed.')
+  data: new SlashCommandBuilder()
+    .setName("mod")
+    .setDescription("Gets current mod embed.")
     .setDefaultMemberPermissions(PermissionFlagsBits.CreateInstantInvite),
-    async execute(interaction, client) {
-        var version = '23w18a'
-        var Iris = 'https://tec-tonic.github.io/snapshot-mods/mods/iris/23w18a/iris-mc23w18a-1.6.2-c075ea36-dirty.jar'
-        var Sodium = 'https://tec-tonic.github.io/snapshot-mods/mods/sodium/23w18a/sodium-fabric-mc23w18a-0.4.11rev.f773c15.jar'
-        
-        const intRestricted = new EmbedBuilder().setDescription(`You do not have access to this command.`).setColor('#FF0000')
+  async execute(interaction, client) {
+    var version = "1.20 pre release 1";
+    var Iris =
+      "https://tec-tonic.github.io/snapshot-mods/mods/iris/1-20-pre-release-1/iris-mc1.20-pre1-1.6.3-b7bf8745-dirty.jar";
+    var Sodium =
+      "https://tec-tonic.github.io/snapshot-mods/mods/sodium/1-20-pre-release-1/sodium-fabric-mc1.20-pre1-0.4.10rev.15f5f00.jar";
 
-        const ModEmbed = new EmbedBuilder().setTitle(version + ' Mods').setColor('#2DF904')
-        .setDescription('For more Snapshot Mods click [here](https://tec-tonic.github.io/snapshot-mods/)')
-        .setFields(
-            {name: `Iris`, value: `[iris-mc23w18a-1.6.2-c075ea36-dirty.jar](${Iris})`},
-            {name: `Sodium`, value: `[sodium-fabric-mc23w18a-0.4.11rev.f773c15.jar](${Sodium})`}
-        )
+    const intRestricted = new EmbedBuilder()
+      .setDescription(`You do not have access to this command.`)
+      .setColor("#FF0000");
 
-        if (
-            interaction.user.id === "546277533138550786" || interaction.user.id === "255766501464735744"
-          ) {
-        interaction.channel.send({embeds: [ModEmbed]})
-        return interaction.reply({content: 'Message Sent', ephemeral: true})
-          } else {
-            return interaction.reply({embeds: [intRestricted],ephemeral: true,})
-          }
+    const ModEmbed = new EmbedBuilder()
+      .setTitle(version + " Mods")
+      .setColor("#2DF904")
+      .setDescription(
+        "For more Snapshot Mods click [here](https://tec-tonic.github.io/snapshot-mods/)"
+      )
+      .setFields(
+        {
+          name: `Iris`,
+          value: `[iris-mc1.20-pre1-1.6.3-b7bf8745-dirty.jar](${Iris})`, //link
+        },
+        {
+          name: `Sodium`,
+          value: `[sodium-fabric-mc1.20-pre1-0.4.10rev.15f5f00.jar](${Sodium})`, //link
+        }
+      );
+
+    if (
+      interaction.user.id === "546277533138550786" ||
+      interaction.user.id === "255766501464735744"
+    ) {
+      interaction.channel.send({ embeds: [ModEmbed] });
+      return interaction.reply({ content: "Message Sent", ephemeral: true });
+    } else {
+      return interaction.reply({ embeds: [intRestricted], ephemeral: true });
     }
-}
+  },
+};
