b6cb2f9f0b9aced2609460d327885d251badddb5
Cody Gordon-Graham <codygg16@gmail.com>
2023-06-07 19:19:39 +0100

    Fixing more of my errors

--- src/events/client/presenceUpdate.js
+++ src/events/client/presenceUpdate.js
@@ -22,19 +22,27 @@ const {
     name: "messageCreate",
     async execute(client) {
 
-  // Updates Presence
-  const axios = require("axios");
-  let url = `https://api.mcstatus.io/v2/status/java/release.pandamium.eu`;
-
-    client.user.setPresence({
-      activities: [
-        {
-          name: `Release: ${server.data.players.online}/${server.data.players.max}`,
-          type: ActivityType.Playing,
-        },
-      ],
-      status: "online",
-    });
+        const axios = require("axios");
+        const ip = "release.pandamium.eu";
+        let url = `https://api.mcstatus.io/v2/status/java/${ip}`;
+        const server = await axios.get(url);
+    
+        try {
+          client.user.setPresence({
+            activities: [
+              {
+                name: `Release: ${server.data.players.online}/${server.data.players.max}`,
+                type: ActivityType.Playing,
+              },
+            ],
+            status: "online",
+          });
+        } catch {
+          client.user.setPresence({
+            activities: [{ name: `Minecraft`, type: ActivityType.Playing }],
+            status: "online",
+          });
+        }
  
     },
   };
