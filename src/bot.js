fd5b0a6055fe258bae73b695a5d4974c199ffbfc
Cody Gordon-Graham <codygg16@gmail.com>
2023-06-07 19:22:25 +0100

    moving it back to main file

--- src/bot.js
+++ src/bot.js
@@ -57,6 +57,29 @@ client.on("messageCreate", async (message) => {
     if (message.author == client.user) return;
     client.command.get(name).execute(message, client);
   }
+// Presence Update
+  const axios = require("axios");
+        const ip = "release.pandamium.eu";
+        let url = `https://api.mcstatus.io/v2/status/java/${ip}`;
+        const server = await axios.get(url);
+    
+        try {
+          client.user.setPresence({
+            activities: [
+              {
+                name: `Release: ${server.data.players.online}/${server.data.players.max}`,
+                type: ActivityType.Playing,
+              },
+            ],
+            status: "online",
+          });
+        } catch {
+          client.user.setPresence({
+            activities: [{ name: `Minecraft`, type: ActivityType.Playing }],
+            status: "online",
+          });
+        }
+
 });
 
 client.eventHandler();
