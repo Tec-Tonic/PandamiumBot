53ee676c3f26371d432c19f4de8bd9418b9526df
Cody Gordon-Graham <codygg16@gmail.com>
2023-06-26 17:53:15 +0100

    fixed month display and removed logs

--- src/commands/tools/leaderboards.js
+++ src/commands/tools/leaderboards.js
@@ -18,101 +18,115 @@ module.exports = {
     .setName("leaderboards")
     .setDescription("Creates leaderboard Embeds")
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-    .addAttachmentOption((option) => 
+    .addAttachmentOption((option) =>
       option
-      .setName('file')
-      .setDescription('please attach file')
-      .setRequired(true)
+        .setName("file")
+        .setDescription("please attach file")
+        .setRequired(true)
     ),
   async execute(interaction, client) {
     // Check user roles
-    if (!interaction.member.roles.cache.some(role => ['Sr. Moderator', 'Admin', 'Owner'].includes(role.name))) {
-      return interaction.reply({content: "You do not have permission to use this command.", ephemeral: true});
+    if (
+      !interaction.member.roles.cache.some((role) =>
+        ["Sr. Moderator", "Admin", "Owner"].includes(role.name)
+      )
+    ) {
+      return interaction.reply({
+        content: "You do not have permission to use this command.",
+        ephemeral: true,
+      });
     }
 
     // Get options
     const upload = interaction.options.getAttachment("file");
-    
-        function getMonthlyData(jsonData) {
-          let result = {
-            monthly_playtime: [],
-            monthly_votes: []
-          };
-          jsonData.monthly_playtime.forEach(item => {
-            result.monthly_playtime.push({
-              username: item.username,
-              value: item.value
-            });
-            console.log('Monthly Playtime:', item.username, item.value);
-          });
-          jsonData.monthly_votes.forEach(item => {
-            result.monthly_votes.push({
-              username: item.username,
-              value: item.value
-            });
-            console.log('Monthly Votes:', item.username, item.value);
-          });
-          return result;
-        }
-    
-        // Fetch file
-        const file = upload.url;
-        if (!file) return console.log('No attached file found');
-    
-        const response = await fetch(file);
-        if (!response.ok)
-          return interaction.channel.send(
-            'There was an error with fetching the file:',
-            response.statusText,
-          );
-        const jsonData = await response.json();
-        let monthlyData = getMonthlyData(jsonData);
-        console.log(monthlyData);
-    
-        const numberEmojis = [' 🥇 ', '🥈', '🥉', '⒋', '⒌', '⒍', '⒎', '⒏', '⒐', '⒑', '⒒', '⒓', '⒔', '⒕', '⒖'];
-        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-        // Helper function to get the end of the month
-        function getMonthEnd(date) {
-          return new Date(date.getFullYear(), date.getMonth() + 1, 0);
-        }
-    
-        // Get closest month end
-        const date = new Date();
-        const closestMonthEnd = getMonthEnd(date);
-        const formattedClosestMonthEnd = `${monthNames[closestMonthEnd.getMonth()]} ${closestMonthEnd.getFullYear()}`;
-    
-        // Create the embeds
-        const PlaytimeEmbed = new EmbedBuilder()
-          .setColor(0x0099FF)
-          .setDescription(`## Monthly Playtime Leaderboard results - ${formattedClosestMonthEnd}`);
-    
-        const VotesEmbed = new EmbedBuilder()
-          .setColor(0x0099FF)
-          .setDescription(`## Monthly Votes Leaderboard results - ${formattedClosestMonthEnd}`);
-    
-        // Add fields for playtime data
-        PlaytimeEmbed.addFields(
-          ...monthlyData.monthly_playtime.map((item, index) => {
-            const hours = Math.floor(item.value * 0.05 / 60 / 60);
-            const minutes = Math.round((item.value * 0.05 / 60 / 60 - hours) * 60);
-            return { name: `${numberEmojis[index]} ${item.username.replace(/_/g, '\\_')}`, value: `\`\`\`${hours} Hrs & ${minutes} Mins\`\`\``, inline: true };
-          })
-        );
-    
-        // Add fields for votes data
-        VotesEmbed.addFields(
-          ...monthlyData.monthly_votes.map((item, index) => ({ name: `${numberEmojis[index]} ${item.username.replace(/_/g, '\\_')}`, value: `\`\`\`${item.value.toString()} Votes\`\`\``, inline: true }))
-        );
-    
-        // Send the embeds
-        interaction.reply({ embeds: [PlaytimeEmbed, VotesEmbed] });
-      }
 
+    function getMonthlyData(jsonData) {
+      let result = {
+        monthly_playtime: [],
+        monthly_votes: [],
+      };
+      jsonData.monthly_playtime.forEach((item) => {
+        result.monthly_playtime.push({
+          username: item.username,
+          value: item.value,
+        });
+      });
+      jsonData.monthly_votes.forEach((item) => {
+        result.monthly_votes.push({
+          username: item.username,
+          value: item.value,
+        });
+      });
+      return result;
     }
-    
-    
- 
-    
 
-       
+    // Fetch file
+    const file = upload.url;
+    if (!file) return console.log("No attached file found");
+
+    const response = await fetch(file);
+    if (!response.ok)
+      return interaction.channel.send(
+        "There was an error with fetching the file:",
+        response.statusText
+      );
+    const jsonData = await response.json();
+    let monthlyData = getMonthlyData(jsonData);
+
+    const numberEmojis = [' 🥇 ', '🥈', '🥉', '⒋', '⒌', '⒍', '⒎', '⒏', '⒐', '⒑', '⒒', '⒓', '⒔', '⒕', '⒖'];
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+    // Helper function to get the end of the month
+    function getMonthEnd(date) {
+      return new Date(date.getFullYear(), date.getMonth(), 0);
+    }
+
+    // Get closest month end
+    const date = new Date();
+    const closestMonthEnd = getMonthEnd(date);
+    const formattedClosestMonthEnd = `${
+      monthNames[closestMonthEnd.getMonth()]
+    } ${closestMonthEnd.getFullYear()}`;
+
+    // Create the embeds
+    const PlaytimeEmbed = new EmbedBuilder()
+      .setColor("#00FF0F")
+      .setDescription(
+        `## Monthly Playtime Leaderboard results - ${formattedClosestMonthEnd}`
+      );
+
+    const VotesEmbed = new EmbedBuilder()
+      .setColor("#00FF0F")
+      .setDescription(
+        `## Monthly Votes Leaderboard results - ${formattedClosestMonthEnd}`
+      );
+
+    // Add fields for playtime data
+    PlaytimeEmbed.addFields(
+      ...monthlyData.monthly_playtime.map((item, index) => {
+        const hours = Math.floor((item.value * 0.05) / 60 / 60);
+        const minutes = Math.round(
+          ((item.value * 0.05) / 60 / 60 - hours) * 60
+        );
+        return {
+          name: `${numberEmojis[index]} ${item.username.replace(/_/g, "\\_")}`,
+          value: `\`\`\`${hours} Hrs & ${minutes} Mins\`\`\``,
+          inline: true,
+        };
+      })
+    );
+
+    // Add fields for votes data
+    VotesEmbed.addFields(
+      ...monthlyData.monthly_votes.map((item, index) => ({
+        name: `${numberEmojis[index]} ${item.username.replace(/_/g, "\\_")}`,
+        value: `\`\`\`${item.value.toString()} Votes\`\`\``,
+        inline: true,
+      }))
+    );
+
+    // Send the embeds
+    interaction.reply({ embeds: [PlaytimeEmbed, VotesEmbed] });
+  },
+};
