3d82fb787d534214071857afe752ffaf995b41b7
Cody Gordon-Graham <codygg16@gmail.com>
2023-07-20 22:16:40 +0100

    fixing puppeteer

--- src/commands/tools/freegames.js
+++ src/commands/tools/freegames.js
@@ -48,7 +48,7 @@ const {
             const { freeNowExpiryDate, freeUpcomingDate } = await getExpiryDate();
       
             async function getExpiryDate() {
-              const browser = await puppeteer.launch({ headless: 'new' });
+              const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
               const page = await browser.newPage();
               await page.goto('https://www.epicgames.com/store/en-US/free-games');
                   
