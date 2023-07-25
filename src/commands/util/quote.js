51954abe29308910f7e8a717f06e8c1fac39b0d0
Cody Gordon-Graham <codygg16@gmail.com>
2023-07-25 09:49:33 +0100

    added more err checks & better msg handler

--- src/commands/util/quote.js
+++ src/commands/util/quote.js
@@ -24,6 +24,16 @@ module.exports = {
           `I do not have access to this message, provide a message link from this server.`
         )
         .setColor("#FF0000");
+        const messageNotFound = new EmbedBuilder()
+        .setDescription(
+          `Message not found, please try again.`
+        )
+        .setColor("#FF0000");
+        const channelNotFound = new EmbedBuilder()
+        .setDescription(
+          `Channel not found, please try again.`
+        )
+        .setColor("#FF0000");
 
       const discordLinkReg = /\/([0-9].*)\/([0-9].*)\/([0-9].*[^/])\/{0,}/;
       const parsed = discordLinkReg.exec(link);
@@ -31,40 +41,98 @@ module.exports = {
       const channelId = parsed[2];
       const guildId = parsed[1];
 
-      if (guildId === "504627012921589763") {
-        const channel = await client.channels.fetch(channelId);
-        const message = await channel.messages.fetch(messageId);
-        const time = Math.round(message.createdTimestamp / 1000);
+      if (guildId === interaction.guild.id) {
+        try {
+          const channel = await client.channels.fetch(channelId);
+          try {
+            const message = await channel.messages.fetch(messageId);
+            const time = Math.round(message.createdTimestamp / 1000);
 
-        const linkReply = new EmbedBuilder()
-          .setAuthor({
-            name: `${message.author.username}`,
-            iconURL:
-              `https://cdn.discordapp.com/avatars/` +
-              message.author.id +
-              `/` +
-              message.author.avatar +
-              `.jpeg`,
-            url: `${link}`,
-          })
-          .setDescription(`${message.content}`)
-          .setColor("#1BEACA");
+            let linkReply;
+            if (message.embeds.length > 0) {
+              // Handle embeds
+              linkReply = new EmbedBuilder(message.embeds[0]);
+            } else {
+              // Handle regular messages
+              linkReply = new EmbedBuilder()
+                .setAuthor({
+                  name: `${message.author.username}`,
+                  iconURL:
+                    `https://cdn.discordapp.com/avatars/` +
+                    message.author.id +
+                    `/` +
+                    message.author.avatar +
+                    `.jpeg`,
+                  url: `${link}`,
+                })
+                .setColor("#1BEACA");
 
-        const dateReply = new EmbedBuilder()
-          .setDescription(
-            `[Original Message](${link}) sent <t:${time}:R> in <#${channelId}>`
-          )
-          .setColor("#1BEACA");
+              if (message.content) {
+                linkReply.setDescription(`${message.content}`);
+              }
 
-        await interaction.reply({ embeds: [dateReply, linkReply] });
+              if (message.attachments.size > 0) {
+                let attachmentText = "";
+                let imageCount = 0;
+                message.attachments.forEach((attachment) => {
+                  if (attachment.contentType.startsWith("image/")) {
+                    imageCount++;
+                    if (imageCount === 1 && message.attachments.size === 1) {
+                      linkReply.setImage(attachment.url);
+                    } else {
+                      attachmentText += `${attachment.name} | [Open Image](${attachment.url})\n`
+                    }
+                  } else {
+                    attachmentText += `${attachment.name} | [Open Image](${attachment.url})\n`;
+                  }
+                });
+                if (attachmentText) {
+                  linkReply.addFields({ name: "Attachments", value: attachmentText });
+                }
+              }
+            }
+
+            const dateReply = new EmbedBuilder()
+              .setDescription(
+                `Original Message sent <t:${time}:R> in <#${channelId}>`
+              )
+              .setColor("#1BEACA");
+
+            await interaction.reply({ embeds: [dateReply, linkReply] });
+          } catch (e) {
+            if (e instanceof DiscordAPIError && e.code === 10008) {
+              // Message not found
+              interaction.reply({
+                embeds: [messageNotFound],
+                ephemeral: true,
+              });
+            } else {
+              throw e;
+            }
+          }
+        } catch (e) {
+          if (e instanceof DiscordAPIError && e.code === 10003) {
+            // Channel not found
+            interaction.reply({
+              embeds: [channelNotFound],
+              ephemeral: true,
+            });
+          } else {
+            throw e;
+          }
+        }
       } else {
-        interaction.reply({ embeds: [ErrlinkReply] });
+        interaction.reply({ embeds: [ErrlinkReply], ephemeral: true });
       }
-    } catch {
-      const Err = new EmbedBuilder()
-        .setColor("#FF0000")
-        .setDescription("Please use a valid link!");
-      interaction.reply({ embeds: [Err], ephemeral: true });
+    } catch (e) {
+      if (!(e instanceof DiscordAPIError)) {
+        console.log(e)
+        // Invalid link
+        const Err = new EmbedBuilder()
+          .setColor("#FF0000")
+          .setDescription("Please use a valid link!");
+        interaction.reply({ embeds: [Err], ephemeral: true });
+      }
     }
   },
 };
