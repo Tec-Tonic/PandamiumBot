d02ae38ba293af95d24be48d3d45e17264b98f5f
Cody Gordon-Graham <codygg16@gmail.com>
2023-08-16 11:27:52 +0100

    fixed vote embed

--- src/events/client/voteEmbed.js
+++ src/events/client/voteEmbed.js
@@ -3,18 +3,17 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
   name: "messageCreate",
   async execute(message, client) {
-    console.log("before code " + message.content)
+    
     if (message.author.bot && message.content.startsWith("**<Rcon>**")) {
-        console.log("after code " + message.content)
+        
       // Extract the username from the message
       const match = message.content.match(/\*\*\<Rcon\>\*\* \[Info\] (\w+) got one vote credit for voting!/);
       if (match) {
         const username = match[1];
 
         // Delete the original message
-        //await message.delete();
-        await message.react('🤖')
-
+        await message.delete();
+        
         // Create an embed version of the message
         const embed = new EmbedBuilder()
           .setColor("#1f8b4c")
