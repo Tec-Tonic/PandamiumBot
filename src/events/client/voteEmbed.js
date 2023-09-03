d4e49017c51f2f266753e98a574ebdff591efd49
cody <codygg16@gmail.com>
2023-09-03 15:17:41 +0200

    vote embed changes

--- src/events/client/voteEmbed.js
+++ src/events/client/voteEmbed.js
@@ -7,26 +7,24 @@ module.exports = {
     if (message.author.bot && message.content.startsWith("**<Rcon>**")) {
         
       // Extract the username and message content from the message
-      const match = message.content.match(/\*\*\<Rcon\>\*\* \[(.+)\] ([\w_]+) (.+)/);
-      if (match) {
-        const category = match[1];
-        const username = match[2];
-        const msgContent = match[3];
+      const splitMessage = message.content.split(" ");
+      const category = splitMessage[2].slice(1, -1);
+      const username = splitMessage[3];
+      const msgContent = splitMessage.slice(4).join(" ");
 
-        // Delete the original message
-        //await message.delete();
-        await message.react("😁");
+      // Delete the original message
+      //await message.delete();
+      await message.react("😁");
+        
+      // Create an embed version of the message
+      const embed = new EmbedBuilder()
+        .setColor("#1f8b4c")
+        .setDescription(
+          `**[${category}]** \`${username}\` ${msgContent}`
+        );
 
-        // Create an embed version of the message
-        const embed = new EmbedBuilder()
-          .setColor("#1f8b4c")
-          .setDescription(
-            `**[${category}]** \`${username}\` ${msgContent}`
-          );
-
-        // Send the embed to the same channel
-        await message.channel.send({ embeds: [embed] });
-      }
+      // Send the embed to the same channel
+      await message.channel.send({ embeds: [embed] });
     }
   },
 };
