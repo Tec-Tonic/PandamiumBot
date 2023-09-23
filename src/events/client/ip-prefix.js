7fba549240e6aee0ec53b6326c49c1c933192fa2
cody <codygg16@gmail.com>
2023-09-23 14:05:32 +0200

    added a prefix ip ask

---
+++ src/events/client/ip-prefix.js
@@ -0,0 +1,39 @@
+const { EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  name: "messageCreate",
+  async execute(client, message) {
+    if (message.content === "!ip") {
+      const url = `https://api.mcstatus.io/v2/status/java/`;
+      const snapshot = await axios.get(url + "snapshot.pandamium.eu");
+      const release = await axios.get(url + "release.pandamium.eu");
+
+      const snapVersion = snapshot.data.version.name_raw;
+      const releaseVersion = release.data.version.name_raw;
+
+      const ipPrefixEmbed = new EmbedBuilder()
+        .setColor("#008000")
+        .setTitle("Pandamium Server IP's")
+        .addFields(
+          {
+            name: `Snapshot IP: `,
+            value: `snapshot.pandamium.eu\n **Version:** ${snapVersion}`,
+          },
+          {
+            name: `Release IP: `,
+            value: `release.pandamium.eu\n **Version:** ${releaseVersion}\n\n`,
+          }
+        );
+
+      message.channel.send({ embeds: [ipPrefixEmbed] }).then((message) => {
+        setTimeout(function () {
+          message.edit(`[Delete] Will be deleted shortly.`);
+        }, 30000);
+        setTimeout(function () {
+          message.delete();
+        }, 32000);
+      });
+    }
+  },
+};
