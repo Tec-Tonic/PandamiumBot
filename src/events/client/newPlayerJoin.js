55c6f7ab5e33b4bfd8cb48b954e19f89677e4feb
cody <codygg16@gmail.com>
2023-09-24 20:12:26 +0200

    fixing some file errors

--- src/events/client/newPlayerJoin.js
+++ src/events/client/newPlayerJoin.js
@@ -6,12 +6,13 @@ module.exports = {
     
     const regex = /<(.*)> \[Server\] Welcome to the server, (.*)! Have fun!/;
     const match = message.content.match(regex);
+    const username = match[1];
     const newPlayerEmbed = new EmbedBuilder()
     .setColor("#00FF04")
     .setDescription(`<@1155559317500596234>\nGuest | ${username} joined the server for the first time!`)
 
+    if (message.author == client.user) return;
     if (match) {
-        const username = match[1];
         client.channels.cache.get("950432522137927690").send({ embeds: [newPlayerEmbed] });
     }
 
