716a5aa2da1a76159688fce976ac3941a8facf4d
cody <codygg16@gmail.com>
2023-10-08 20:48:09 +0200

    readding loop

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -53,91 +53,91 @@ module.exports = {
     }
 
     // // Loop
-    // setInterval(playerlistUpdate, 300000); //Every 5 min.
+    setInterval(playerlistUpdate, 300000); //Every 5 min.
 
-    // async function playerlistUpdate() {
-    //   const remove = require("../../functions/events/punctuation");
+    async function playerlistUpdate() {
+      const remove = require("../../functions/events/punctuation");
 
-    //   const utility = require("minecraft-server-util");
-    //   const axios = require("axios");
-    //   const util = require("util");
-    //   const url = `https://api.mcstatus.io/v2/status/java/snapshot.pandamium.eu`;
-    //   const server = await axios.get(url);
-    //   const search = util.inspect;
-    //   const checkIfPlayer = server.data.players.online;
-    //   const unixTimestamp = Math.floor((Date.now() + 300000) / 1000);
+      const utility = require("minecraft-server-util");
+      const axios = require("axios");
+      const util = require("util");
+      const url = `https://api.mcstatus.io/v2/status/java/snapshot.pandamium.eu`;
+      const server = await axios.get(url);
+      const search = util.inspect;
+      const checkIfPlayer = server.data.players.online;
+      const unixTimestamp = Math.floor((Date.now() + 300000) / 1000);
 
-    //   // Message to Update
-    //   const msgID = "1138257210925924403";
-    //   const channelID = "824234748217393212";
+      // Message to Update
+      const msgID = "1160649523631243395";
+      const channelID = "824234748217393212";
 
-    //   const channel = client.channels.cache.get(channelID);
+      const channel = client.channels.cache.get(channelID);
 
-    //   const options = {
-    //     sessionID: 1, // a random 32-bit signed number, optional
-    //     enableSRV: true, // SRV record lookup
-    //   };
+      const options = {
+        sessionID: 1, // a random 32-bit signed number, optional
+        enableSRV: true, // SRV record lookup
+      };
 
-    //   try {
-    //     //Checks if the server is Empty or not
+      try {
+        //Checks if the server is Empty or not
 
-    //     if (checkIfPlayer.toString() === "0") {
-    //       const ServerEmpty = new EmbedBuilder()
-    //         .setColor("#FF0000")
-    //         .setTitle(`**Server is Empty**`)
-    //         .setDescription(`*Message will refresh <t:${unixTimestamp}:R>*`)
-    //         .setFooter({ text: `Version: ${server.data.version.name_raw}` });
+        if (checkIfPlayer.toString() === "0") {
+          const ServerEmpty = new EmbedBuilder()
+            .setColor("#FF0000")
+            .setTitle(`**Server is Empty**`)
+            .setDescription(`*Message will refresh <t:${unixTimestamp}:R>*`)
+            .setFooter({ text: `Version: ${server.data.version.name_raw}` });
 
-    //       channel.messages
-    //         .fetch(msgID)
-    //         .then((msg) =>
-    //           msg.edit({
-    //             content:
-    //               "# The chat linking mod we have been using is currently broken in the latest Minecraft version. Keep an eye on <#505093367903027220> to know when this channel is working again. \n## You can use </playerlist:1102727613102960663> in <#614507998357880862>.",
-    //             embeds: [ServerEmpty],
-    //           })
-    //         );
-    //     } else {
-    //       utility.queryFull("pandamium.eu", 25566, options).then((Response) => {
-    //         const nameArr = Response.players.list.join(", ").toString();
+          channel.messages
+            .fetch(msgID)
+            .then((msg) =>
+              msg.edit({
+                content:
+                  "# The chat linking mod we have been using is currently broken in the latest Minecraft version. Keep an eye on <#505093367903027220> to know when this channel is working again. \n## You can use </playerlist:1102727613102960663> in <#614507998357880862>.",
+                embeds: [ServerEmpty],
+              })
+            );
+        } else {
+          utility.queryFull("pandamium.eu", 25566, options).then((Response) => {
+            const nameArr = Response.players.list.join(", ").toString();
 
-    //         const playerlistEmbed = new EmbedBuilder()
-    //           .setColor("#2DF904")
-    //           .setTitle(
-    //             `**Online players (${Response.players.online}/${Response.players.max}):**`
-    //           )
-    //           .setDescription(
-    //             `\`\`\`${nameArr}\`\`\`\n*Message will refresh <t:${unixTimestamp}:R>*`
-    //           )
-    //           .setFooter({ text: `Version: ${server.data.version.name_raw}` });
+            const playerlistEmbed = new EmbedBuilder()
+              .setColor("#2DF904")
+              .setTitle(
+                `**Online players (${Response.players.online}/${Response.players.max}):**`
+              )
+              .setDescription(
+                `\`\`\`${nameArr}\`\`\`\n*Message will refresh <t:${unixTimestamp}:R>*`
+              )
+              .setFooter({ text: `Version: ${server.data.version.name_raw}` });
 
-    //         channel.messages
-    //           .fetch(msgID)
-    //           .then((msg) =>
-    //             msg.edit({
-    //               content:
-    //                 "# The chat linking mod we have been using is currently broken in the latest Minecraft version. Keep an eye on <#505093367903027220> to know when this channel is working again. \n## You can use </playerlist:1102727613102960663> in <#614507998357880862>.",
-    //               embeds: [playerlistEmbed],
-    //             })
-    //           );
-    //       });
-    //     }
-    //   } catch {
-    //     const serverOffline = new EmbedBuilder()
-    //       .setColor("#FF0000")
-    //       .setTitle(`**Server is offline **`)
-    //       .setDescription(`*Message will refresh <t:${unixTimestamp}:R>*`);
+            channel.messages
+              .fetch(msgID)
+              .then((msg) =>
+                msg.edit({
+                  content:
+                    "# The chat linking mod we have been using is currently broken in the latest Minecraft version. Keep an eye on <#505093367903027220> to know when this channel is working again. \n## You can use </playerlist:1102727613102960663> in <#614507998357880862>.",
+                  embeds: [playerlistEmbed],
+                })
+              );
+          });
+        }
+      } catch {
+        const serverOffline = new EmbedBuilder()
+          .setColor("#FF0000")
+          .setTitle(`**Server is offline **`)
+          .setDescription(`*Message will refresh <t:${unixTimestamp}:R>*`);
 
-    //     channel.messages
-    //       .fetch(msgID)
-    //       .then((msg) =>
-    //         msg.edit({
-    //           content:
-    //             "# The chat linking mod we have been using is currently broken in the latest Minecraft version. Keep an eye on <#505093367903027220> to know when this channel is working again. \n## You can use </playerlist:1102727613102960663> in <#614507998357880862>.",
-    //           embeds: [serverOffline],
-    //         })
-    //       );
-    //   }
-    // }
+        channel.messages
+          .fetch(msgID)
+          .then((msg) =>
+            msg.edit({
+              content:
+                "# The chat linking mod we have been using is currently broken in the latest Minecraft version. Keep an eye on <#505093367903027220> to know when this channel is working again. \n## You can use </playerlist:1102727613102960663> in <#614507998357880862>.",
+              embeds: [serverOffline],
+            })
+          );
+      }
+    }
   },
 };
