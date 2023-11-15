0efc940c8f7a27666bb32532b21fecb7b449a4b2
Cody Gordon-Graham <codygg16@gmail.com>
2023-11-15 18:53:30 +0000

    Added a fix for 2000 character error in /suggest

--- src/events/client/suggestCreate.js
+++ src/events/client/suggestCreate.js
@@ -1,28 +1,65 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SelectMenuBuilder, EmbedBuilder, Client, GatewayIntentBits, ActivityType, Routes, Message, messageLink, Collection, channelLink, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
-
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  SelectMenuBuilder,
+  EmbedBuilder,
+  Client,
+  GatewayIntentBits,
+  ActivityType,
+  Routes,
+  Message,
+  messageLink,
+  Collection,
+  channelLink,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
 
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction, client) {
-
+  name: "interactionCreate",
+  async execute(interaction, client) {
     if (interaction.customId === "suggest") {
+      let suggestion = interaction.fields.getTextInputValue("suggestInput");
 
-      const suggestion = interaction.fields.getTextInputValue("suggestInput");
+      // Check if the suggestion text is more than 2000 characters
+      if (suggestion.length > 2000) {
+        let breakPoint = suggestion.slice(0, 2000).lastIndexOf(" ");
+        const firstPart = suggestion.slice(0, breakPoint);
+        const secondPart = suggestion.slice(breakPoint + 1);
 
-      const channel = client.channels.cache.get("1137133652086636584");
-      const thread = await channel.threads.create({
-        name: "suggestion-thread",
-        autoArchiveDuration: 60,
-        reason: "Needed a separate thread for suggestions",
-      });
-      console.log(`Created thread: ${thread.name}`);
+        const channel = client.channels.cache.get("1137153655477391421");
+        const thread = await channel.threads.create({
+          name: "suggestion-thread",
+          autoArchiveDuration: 60,
+          reason: "Needed a separate thread for suggestions",
+        });
+        console.log(`Created thread: ${thread.name}`);
 
-      await thread.send(suggestion);
+        await thread.send(firstPart);
+        await thread.send(secondPart);
 
-      return interaction.reply({
-        content: "Your suggestion was sent!",
-        ephemeral: true,
-      });
+        return interaction.reply({
+          content: "Your suggestion was sent!",
+          ephemeral: true,
+        });
+      } else {
+        const channel = client.channels.cache.get("1137153655477391421");
+        const thread = await channel.threads.create({
+          name: "suggestion-thread",
+          autoArchiveDuration: 60,
+          reason: "Needed a separate thread for suggestions",
+        });
+        console.log(`Created thread: ${thread.name}`);
+
+        await thread.send(suggestion);
+
+        return interaction.reply({
+          content: "Your suggestion was sent!",
+          ephemeral: true,
+        });
+      }
     }
   },
 };
