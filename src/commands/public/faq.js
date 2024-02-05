592561e19a4ec015bfe87d4dd46b938fa07a3773
Cody Gordon-Graham <codygg16@gmail.com>
2024-02-05 23:04:22 +0000

    Added new command to FAQ & Public boolean option

--- src/commands/public/faq.js
+++ src/commands/public/faq.js
@@ -22,11 +22,20 @@ module.exports = {
           { name: "Connection Issues", value: "connect-faq" },
           { name: "Last World Reset (Snapshot)", value: "reset-faq" },
           { name: "Server IP", value: "ip-faq" },
+          { name: "Supporter Rank", value: "support-faq" }
         )
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("public")
+        .setDescription("Set embed to be publicly visible")
+        .setRequired(false)
     ),
 
   async execute(interaction, client) {
     const intAuth = interaction.user.tag; //if needed
+    let showPublic = interaction.options.getBoolean("public");
+    showPublic = showPublic !== null ? !showPublic : true;
 
     // No access
     const Blocked = new EmbedBuilder()
@@ -35,6 +44,7 @@ module.exports = {
     const revoke = interaction.member.roles.cache.some(
       (r) => r.name === "NoSNapshotIngameChat"
     );
+
     if (revoke)
       return interaction.reply({ embeds: [Blocked], ephemeral: true });
 
@@ -47,14 +57,15 @@ module.exports = {
         .setDescription(
           `Possible ways to fix connection issues (snapshot server):\n• Clear your dns cache (if you’re on Windows, try the command \`ipconfig /flushdns\`)\n• Use the IP \`pandamium.eu:25566\`\n• Use the IP \`116.202.216.130:25566\`\n• Try using a VPN\n• Wait it out - it may be a problem with your ISP who will fix the problem themselves (try again in a few hours)\n• https://help.minecraft.net/hc/en-us/articles/360034754052-Multiplayer-Connection-Issues-Java-Edition-`
         );
-      return interaction.reply({ embeds: [connectionEmbed] });
+      return interaction.reply({ embeds: [connectionEmbed], ephemeral: showPublic });
     }
 
     // Update Game versions
     else if (category === "gameVer-faq") {
-      return interaction.reply(
-        "https://help.minecraft.net/hc/en-us/articles/360034754852-Changing-game-versions-"
-      );
+      return interaction.reply({content:
+        "https://help.minecraft.net/hc/en-us/articles/360034754852-Changing-game-versions-",
+        ephemeral: showPublic
+      });
     }
 
     // Reset Info
@@ -65,10 +76,8 @@ module.exports = {
           `Last reset : 26th September 2021\n\nThe Snapshot server was reset due to the world not being able to updated from [21W37A](https://www.minecraft.net/en-us/article/minecraft-snapshot-21w37a) to [21W38A](https://www.minecraft.net/en-us/article/minecraft-snapshot-21w38a) after Mojang added the new world generation. All dimensions were reset. \nWe always keep player data (e.g. Playtime, Votes and inventories) `
         );
 
-      return interaction.reply({ embeds: [WorldResetEmbed] });
-    }
-
-    else if (category === "ip-faq") {
+      return interaction.reply({ embeds: [WorldResetEmbed], ephemeral: showPublic });
+    } else if (category === "ip-faq") {
       const SnapshotURL = `https://api.mcstatus.io/v2/status/java/snapshot.pandamium.eu`;
       const ReleaseURL = `https://api.mcstatus.io/v2/status/java/release.pandamium.eu`;
       const axios = require("axios");
@@ -76,20 +85,33 @@ module.exports = {
       const ReleaseServer = await axios.get(ReleaseURL);
 
       const ipEmbed = new EmbedBuilder()
-          .setColor("#008000")
-          .setTitle("Pandamium Server IP's")
-          .addFields(
-            {
-              name: `Snapshot IP: `,
-              value: `snapshot.pandamium.eu\n **Version:** ${SnapshotServer.data.version.name_raw}`,
-            },
-            {
-              name: `Release IP: `,
-              value: `release.pandamium.eu\n **Version:** ${ReleaseServer.data.version.name_raw}\n\n`,
-            }
-          )
+        .setColor("#008000")
+        .setTitle("Pandamium Server IP's")
+        .addFields(
+          {
+            name: `Snapshot IP: `,
+            value: `snapshot.pandamium.eu\n **Version:** ${SnapshotServer.data.version.name_raw}`,
+          },
+          {
+            name: `Release IP: `,
+            value: `release.pandamium.eu\n **Version:** ${ReleaseServer.data.version.name_raw}\n\n`,
+          }
+        );
 
-          return interaction.reply({ embeds: [ipEmbed], ephemeral: true });
+      return interaction.reply({ embeds: [ipEmbed], ephemeral: showPublic });
+    } else if (category === "support-faq") {
+      const SupporterEmbed = new EmbedBuilder()
+        .setTitle(`Supporter Rank`)
+        .setColor(`#00FFD8`).setDescription(`
+We have changed the "Donator" rank to "Supporter". Nothing is changing with the cosmetic perks that come with the rank, and if you already have the rank then you will of course keep it, this is purely a visual change.
+### Why?
+In order to appear on the [official Minecraft server list](https://findmcserver.com/server/GiOOKwFVlz?vote=true) we have to comply with Mojang's usage and community and guidelines. We already do for the most part, so nothing else will be changing about the server, but the specific use of the word "donate" and its variants come with legal implications that we'd like to avoid. 
+`);
+
+      return interaction.reply({
+        embeds: [SupporterEmbed],
+        ephemeral: showPublic,
+      });
     }
   },
 };
