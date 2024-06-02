8fb0600bbf5c4adc9b8929e3398f51838b8f96e0
Tec <87786660+Tec-Tonic@users.noreply.github.com>
2024-06-02 22:16:05 +0100

    fixing bot return err

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -21,12 +21,15 @@ const {
 module.exports = {
   name: "messageCreate",
   async execute(message, client) {
-
     // Sends ping if new Snapshot is detected
     const MojiraUpdateFilter = require("../../chatAlert/filters/mojira_ping_filter.json");
     let MojiraUpdatePing = false;
     for (var i in MojiraUpdateFilter) {
-      if (message.content.toLowerCase().includes(MojiraUpdateFilter[i].toLowerCase()))
+      if (
+        message.content
+          .toLowerCase()
+          .includes(MojiraUpdateFilter[i].toLowerCase())
+      )
         if (message.channel.name === "minecraft-updates")
           if (message.author.bot) {
             MojiraUpdatePing = true;
@@ -41,11 +44,11 @@ module.exports = {
     const MojiraFilter = require("../../chatAlert/filters/mojira_delete_filter.json");
     let MojiraFoundInText = false;
     for (var i in MojiraFilter) {
-        if (message.content.toLowerCase().includes(MojiraFilter[i].toLowerCase()))
-            if (message.channel.name === 'minecraft-updates')
-                if (message.author.bot) {
-          MojiraFoundInText = true;
-        }
+      if (message.content.toLowerCase().includes(MojiraFilter[i].toLowerCase()))
+        if (message.channel.name === "minecraft-updates")
+          if (message.author.bot) {
+            MojiraFoundInText = true;
+          }
     }
 
     if (MojiraFoundInText) {
@@ -59,16 +62,24 @@ module.exports = {
 
     // If server info is sent to discord, delete
     const inGameChatFilter = require("../../chatAlert/filters/chat_delete_filter.json");
-    const remove = require('../../functions/events/punctuation')
+    const remove = require("../../functions/events/punctuation");
 
     let TextFoundInText = false;
-    const reminder = message.content
+    const reminder = message.content;
     for (var i in inGameChatFilter) {
-        if (remove.Punctuation(reminder).toLowerCase().includes(remove.Punctuation(inGameChatFilter[i].toLowerCase())))
-            if (message.channel.name === 'snapshot-ingame-chat')
-                if (message.author.bot || message.author.id === "546277533138550786") {
-          TextFoundInText = true;
-        }
+      if (
+        remove
+          .Punctuation(reminder)
+          .toLowerCase()
+          .includes(remove.Punctuation(inGameChatFilter[i].toLowerCase()))
+      )
+        if (message.channel.name === "snapshot-ingame-chat")
+          if (
+            message.author.bot ||
+            message.author.id === "546277533138550786"
+          ) {
+            TextFoundInText = true;
+          }
     }
 
     if (TextFoundInText) {
@@ -77,6 +88,52 @@ module.exports = {
       setTimeout(() => {
         return message.delete();
       }, 5000);
+    }
+    // ^^^
+
+    // Vote Message Embed Creator
+    if (
+      message.author.bot &&
+      (message.content.startsWith("*Rcon") ||
+        message.content.startsWith("*Server"))
+    ) {
+      // Extract the username and reward credits from the message
+      const match = message.content.match(
+        /(?:Rcon|Server) \[Voting\] ([^\s]+) got (\d+) reward/
+      );
+      if (match) {
+        const username = match[1];
+        const credits = match[2];
+        const creditWord = credits > 1 ? "credits" : "credit";
+
+        // Delete the original message
+        await message.delete();
+
+        // Create an embed version of the message
+        const embed = new EmbedBuilder()
+          .setColor("#1f8b4c")
+          .setDescription(
+            `**[Voting]** ${username} got ${credits} reward ${creditWord} for voting!`
+          );
+
+        // Send the embed to the same channel
+        await message.channel.send({ embeds: [embed] });
+      }
+
+      // Double Vote Credit Week
+      const DVCmatch = message.content.includes(
+        "The monthly leader boards have been reset and a week of double reward credits for voting has begun!"
+      );
+
+      const channel = client.channels.cache.get("1095405945917550623");
+
+      if (channel && DVCmatch) {
+        channel.send(
+          "<@&505093367903027220>\n# Double Voting Rewards Week!\nFor the next week, you will now receive 2 reward credits when voting for the **snapshot** server!"
+        );
+      } else {
+        console.log("Channel not found");
+      }
     }
     // ^^^
 
@@ -90,63 +147,32 @@ module.exports = {
       .setDescription(
         `Hey ${author.username}, \nIf you require help please use the help button below and a Staff member will respond when available. \n\nHappy Minecrafting, Pandamium`
       );
-    
+
     const codeEmbed = new EmbedBuilder()
       .setColor("#F205FA")
       .setTitle(`**Direct Message Received**`)
       .setDescription(`${author.username} sent:\n${DMmessage}`);
 
-      const button = new ButtonBuilder()
-        .setCustomId(`dmHelp`)
-        .setLabel(`Help`)
-        .setStyle(ButtonStyle.Danger)
-        .setDisabled(false);
+    const button = new ButtonBuilder()
+      .setCustomId(`dmHelp`)
+      .setLabel(`Help`)
+      .setStyle(ButtonStyle.Danger)
+      .setDisabled(false);
 
     if (message.author.bot) return;
     if (message.channel.type === ChannelType.DM) {
       if (!DMmessage) return;
-      message.reply({ embeds: [rejectMessage], components: [new ActionRowBuilder().addComponents(button)] });
-      
-      client.channels.cache.get("1095405945917550623").send({ embeds: [codeEmbed] }); //LOGS BOT SERVER
+      message.reply({
+        embeds: [rejectMessage],
+        components: [new ActionRowBuilder().addComponents(button)],
+      });
+
+      client.channels.cache
+        .get("1095405945917550623")
+        .send({ embeds: [codeEmbed] }); //LOGS BOT SERVER
     }
 
-    module.exports.author = author
+    module.exports.author = author;
     // ^^^
-
-    // Vote Message Embed Creator
-    if (message.author.bot && (message.content.startsWith("*Rcon") || message.content.startsWith("*Server"))) {
-        
-      // Extract the username and reward credits from the message
-      const match = message.content.match(/(?:Rcon|Server) \[Voting\] ([^\s]+) got (\d+) reward/);
-      if (match) {
-        const username = match[1];
-        const credits = match[2];
-        const creditWord = credits > 1 ? 'credits' : 'credit';
-
-        // Delete the original message
-        await message.delete();
-        
-        // Create an embed version of the message
-        const embed = new EmbedBuilder()
-          .setColor("#1f8b4c")
-          .setDescription(
-            `**[Voting]** ${username} got ${credits} reward ${creditWord} for voting!`
-          );
-
-        // Send the embed to the same channel
-        await message.channel.send({ embeds: [embed] });
-      }
-
-      // Double Vote Credit Week
-      const DVCmatch = message.content.includes("The monthly leader boards have been reset and a week of double reward credits for voting has begun!")
-        
-        const channel = client.channels.cache.get('1095405945917550623');
-        
-        if (channel && DVCmatch) {
-          channel.send('<@&505093367903027220>\n# Double Voting Rewards Week!\nFor the next week, you will now receive 2 reward credits when voting for the **snapshot** server!');
-        } else {
-          console.log('Channel not found');
-        }
-    }
   },
 };
